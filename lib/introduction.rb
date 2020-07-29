aa2005af7adc4b9d33245ec1d1ecbc90b85c61c8
Brian Donnoe <dunow33@hotmail.com>
2020-07-29 04:20:43 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-#def two_arg(name, age)
+def introduction_with_language(name, age)
 #  puts "Hello #{name}, your age is #{age} years old"
 #end
