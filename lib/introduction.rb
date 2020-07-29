272247ccc864b304af762c82209560973c55075f
Brian Donnoe <dunow33@hotmail.com>
2020-07-29 04:20:24 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,12 +2,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-#introduction("Briguy")
-
 #def two_arg(name, age)
 #  puts "Hello #{name}, your age is #{age} years old"
 #end
-
-#one_arg("Briguy")
-
-#two_arg("Briguy", 41)
